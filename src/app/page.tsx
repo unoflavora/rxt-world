168f7107ffc4268fb9f6ffcb89d12e42a717adba
Imam Syahid Hudzaifah <imaamsyahid@gmail.com>
2023-10-24 07:22:35 +0700

    add container header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,23 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between container"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="w-full h-screen md:h-[32rem] xl:h-[50rem] bg-[url('/bg/grid.svg')] bg-cover bg-center bg-no-repeat flex justify-center bg-fixed ">
+        <div className="container px-4 py-2 grid grid-rows-2 grid-flow-row">
+          <article className="py-9 flex flex-col text-center gap-6 justify-center items-center  font-semibold h-full ">
+            <h1 className="text-xl md:text-3xl xl:text-6xl">
+              Bitcoin Land: RXT&apos;s Vision for Malaysia
+            </h1>
+            <h2 className="md:text-2xl xl:text-4xl">
+              Transforming the Penang, Malaysia Waterfront into Asia&apos;s
+              Premier Tech Hub
+            </h2>
+          </article>
+          <div className="bg-[#262217] flex w-full h-[150%] justify-center  text-center items-center">
+            <h1 className="text-4xl">GIF</h1>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
--- src/components/ui/navbar/navbar.tsx
+++ src/components/ui/navbar/navbar.tsx
@@ -1,10 +1,17 @@
+"use client";
 import Link from "next/link";
 import Navmenu from "./NavMenu";
 import NavMenuMobile from "./NavMenuMobile";
+import { useVisibleOnScrollUp } from "@/hooks/useVisibleScroll";
 
 export default function Navbar() {
+  const visible = useVisibleOnScrollUp();
   return (
-    <nav className="w-full ">
+    <nav
+      className={`fixed w-full transition-transform duration-300 ${
+        visible ? "translate-y-0" : "-translate-y-24"
+      }`}
+    >
       <div className="w-full container py-5 flex justify-between  ">
         <Link
           href="/"
