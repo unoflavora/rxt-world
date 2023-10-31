2a46211ee74609815f0f2595fd48287b5ea87cb1
Imam Syahid Hudzaifah <imaamsyahid@gmail.com>
2023-10-31 22:57:12 +0700

    adjust logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="flex items-center gap-1 text-white font-semibold sm:py-1 lg:text-[1.5rem] lg:leading-9 ">
+    <div className="flex items-center gap-1 text-white font-semibold sm:py-1 lg:text-[1.5rem] lg:leading-9 font-sans ">
       <Image
         src={"/company/token.png"}
         width={30}
         height={25}
         alt="company logo"
       />
-      <h1>BITCOINLAND</h1>
+      <h1>RXT CAPITAL LIMITED</h1>
     </div>
   );
 }
