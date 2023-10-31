2b79da23f001108fdfc40cb04f959029effb2501
Imam Syahid Hudzaifah <imaamsyahid@gmail.com>
2023-10-31 19:54:49 +0700

    remove bg white

--- src/app/(home-components)/SeeBitcoin.tsx
+++ src/app/(home-components)/SeeBitcoin.tsx
@@ -10,7 +10,7 @@ export default function Bitcoinland() {
           BITCOIN LAND
         </div>
         <div className="w-full relative justify-center items-end text-4xl">
-          <div className="w-full h-[46rem] bg-white">
+          <div className="w-full h-[46rem] ">
             <MapScene />
           </div>
           <div className="absolute -bottom-7 left-1/2 -translate-x-1/2 bg-background w-[80%] max-w-[46rem]  py-7 px-14 flex max-sm:flex-col max-sm:justify-center max-sm:gap-2 justify-between items-center">
