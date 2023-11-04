53f1b9e55d1ea18b5697ef60f30641763f828341
Imam Syahid Hudzaifah <imaamsyahid@gmail.com>
2023-11-04 11:22:07 +0700

    adjust news

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -3,18 +3,25 @@ import { NewsType } from "@/types/news";
 import Link from "next/link";
 import { News } from "./components/news";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const news = await client.fetch<NewsType[]>(`*[_type == "news"]`, {
-    next: {
-      revalidate: 1,
-    },
+    cache: "no-store",
   });
 
   return (
-    <div className="container  gap-14 p-24 flex flex-col ">
+    <div className="container  gap-14 lg:px-24 flex flex-col ">
       <h1 className="text-2xl font-bold">Company News</h1>
       {news.map((content) => {
-        return <News key={content._id} content={content} fixedSize />;
+        return (
+          <News
+            key={content._id}
+            content={content}
+            fixedSize
+            className="md:grid md:grid-cols-2"
+          />
+        );
       })}
     </div>
   );
