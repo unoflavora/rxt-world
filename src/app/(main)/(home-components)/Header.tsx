cd9a9294dc3ba6e1c8b5b1f1eb0a7b3d99ecce78
Imam Syahid Hudzaifah <me@imamsyahid.dev>
2023-12-11 14:23:48 +0700

    chore: smoothing the scaling up of header

--- src/app/(main)/(home-components)/Header.tsx
+++ src/app/(main)/(home-components)/Header.tsx
@@ -9,15 +9,18 @@ export default function HomeHeader(props: { fileId: string }) {
 
   const { scrollYProgress } = useScroll({
     target: headerRef,
-    offset: [0, "100vh"],
+    offset: ["-10vh", "100vh"],
   });
 
   const [currentDivProgress, setCurrentDivProgress] = useState(0);
 
   useEffect(() => {
-    scrollYProgress.on("change", (val) => {
-      setCurrentDivProgress(Math.min(1 + val, 1.4));
-    });
+    const handleScroll = (val: number) => {
+      setCurrentDivProgress(Math.min(1 + val, 1.5));
+    };
+
+    scrollYProgress.on("change", handleScroll);
+
     return () => {
       scrollYProgress.clearListeners();
     };
@@ -29,16 +32,15 @@ export default function HomeHeader(props: { fileId: string }) {
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   });
-  console.log(url);
 
   return (
     <div
       ref={headerRef}
-      className="w-full min-h-fit py-20 bg-[url('/bg/grid.svg')] overflow-hidden  bg-cover bg-center bg-no-repeat flex  justify-center items-center  bg-fixed "
+      className="w-full min-h-fit py-20 bg-[url('/bg/grid.svg')] overflow-hidden bg-cover bg-center bg-no-repeat flex justify-center items-center bg-fixed"
     >
-      <div className="relative container z-10 px-4 flex flex-col gap-10  ">
-        <article className="lg:py-36 flex flex-col text-center gap-6 justify-center items-center  font-semibold  ">
-          <h1 className="text-2xl md:text-3xl xl:text-6xl font-crimson">
+      <div className="relative container z-10 px-4 flex flex-col gap-10">
+        <article className=" lg:py-36 flex flex-col text-center gap-6 justify-center items-center font-semibold">
+          <h1 className=" text-2xl md:text-3xl xl:text-6xl font-crimson">
             Bitcoinland: RXT&apos;s Vision for Malaysia
           </h1>
           <h2 className="text-lg md:text-2xl xl:text-4xl text-tertiary">
@@ -48,7 +50,7 @@ export default function HomeHeader(props: { fileId: string }) {
         </article>
         <video
           preload="true"
-          style={{ scale: Math.max(1, currentDivProgress) }}
+          style={{ transform: `scale(${Math.max(1, currentDivProgress)})` }}
           src={url}
           autoPlay
           loop
